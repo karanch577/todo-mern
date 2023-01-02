7f08cdc81ceb7d1fe3f6b6b3a9e5a398128dbdef
Karan Chauhan <karandemo577@gmail.com>
2023-01-02 19:51:58 +0530

    baseUrl added

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import axios from "axios";
+
+axios.defaults.baseURL = "https://todo-41wb.onrender.com:4000"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
