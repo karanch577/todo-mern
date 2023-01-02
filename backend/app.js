24013b774b10bf54f3e7442f6aab96f92ad6ab96
Karan Chauhan <karandemo577@gmail.com>
2023-01-02 19:21:59 +0530

    cors added

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const app = express()
 const cookieParser = require("cookie-parser")
+const cors = require("cors")
 const todoRoutes = require("./routes/todo")
 const taskRoutes = require("./routes/task")
 const userRoutes = require("./routes/user")
@@ -13,6 +14,7 @@ connectToDb()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true })) 
 app.use(cookieParser())
+app.use(cors())
 
 // routes
 app.use("/", todoRoutes)
